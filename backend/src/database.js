23a2f82c34fb008ea1b66c10687a57f01e3657b4
Yeison Rojas <yeisonrojas03@gmail.com>
2025-12-07 03:08:15 -0400

    Switch to in-memory SQLite database
    Changed database connection to use in-memory SQLite and updated connection message.

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,13 +1,7 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database('./db.sqlite', (err) => {
-  if (err) console.error('Error abriendo BD:', err);
-  else console.log('SQLite conectado');
+const db = new sqlite3.Database(":memory:", () => {
+  console.log("SQLite conectado");
 });
-
-db.run(`CREATE TABLE IF NOT EXISTS items (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL
-)`);
 
 module.exports = db;
